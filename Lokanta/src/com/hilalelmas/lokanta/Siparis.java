4699e083acc2655673c2c75eb363486f5071b467
Sony <Sony@Sony-Bilgisayar>
2017-06-20 02:23:18 +0300

    mvn

---
+++ Lokanta/src/com/hilalelmas/lokanta/Siparis.java
@@ -0,0 +1,18 @@
+package com.hilalelmas.lokanta;
+
+public class Siparis {
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void siparisAl() {
+		System.out.println(name + " sipariþ verdi");
+	}
+
+}
---
+++ Musteri/src/com/hilalelmas/musteri/Islem.java
@@ -0,0 +1,13 @@
+package com.hilalelmas.musteri;
+
+import com.hilalelmas.lokanta.Siparis;
+
+public class Islem {
+	public static void main(String[] args) {
+		Siparis siparis = new Siparis();
+		siparis.setName("hilal");
+		siparis.siparisAl();
+
+	}
+
+}
