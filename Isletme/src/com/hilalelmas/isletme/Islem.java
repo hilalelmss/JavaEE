489f0fbcd6a10c7eba28a64b45fb8fb0fb473d88
Sony <Sony@Sony-Bilgisayar>
2017-06-20 02:02:13 +0300

    maven

---
+++ Isletme/src/com/hilalelmas/isletme/Islem.java
@@ -0,0 +1,12 @@
+package com.hilalelmas.isletme;
+
+import com.hilal.marka.Fabrika;
+
+public class Islem {
+public static void main(String[] args) {
+	
+	Fabrika  fabrika=new Fabrika();
+	fabrika.markaUret();
+	
+}
+}
